173a0e1d26ac15383136889d277e3b4415e0ef53
Ben S <benslinde@gmail.com>
2021-09-17 11:55:25 -0400

    coded index.js for model associations

--- models/index.js
+++ models/index.js
@@ -6,14 +6,40 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', 
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+  as: "tag",
+  foreignKey: "product_id",
+});
+
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  as: "product",
+  foreignKey: "tag_id",
+});
 
 
+
+//This is how sequelize docs are doing this but the context is all wrong
+// User.belongsToMany(Profile, { through: Grant });
+// Profile.belongsToMany(User, { through: Grant });
+// User.hasMany(Grant);
+// Grant.belongsTo(User);
+// Profile.hasMany(Grant);
+// Grant.belongsTo(Profile);
 
 
 
